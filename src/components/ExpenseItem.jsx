c5b0af085921d9b96607542eeb05110c6c111060
pratsy91 <pratapsinghchouhan412@gmail.com>
2022-12-21 23:30:17 +0530

    expenseitems component

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -7,6 +7,7 @@ const ExpenseItem = (props) => {
       <ExpenseDate edate={props.edate} />
       <div className="expense-item__description">
         <h2>{props.etitle}</h2>
+        <div>Location:{props.eloe}</div>
         <div className="expense-item__price">{props.eamount}</div>
       </div>
     </div>
